2b7d2f79954505cc8f859191307be74e3e944e24
Nugine <nugine@foxmail.com>
2024-11-20 16:01:05 +0800

    feat: parse

---
+++ scripts/crawl.py
@@ -0,0 +1,98 @@
+# 中华人民共和国行政区划代码
+# https://www.mca.gov.cn/n156/n186/index.html
+
+from pathlib import Path
+import re
+import time
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+URL_INDEX = {
+    2022: "https://www.mca.gov.cn/mzsj/xzqh/2022/202201xzqh.html",
+    2021: "https://www.mca.gov.cn/mzsj/xzqh/2021/20211201.html",
+    2020: "https://www.mca.gov.cn/mzsj/xzqh/2020/20201201.html",
+    2019: "https://www.mca.gov.cn/mzsj/xzqh/1980/2019/202002281436.html",
+    2018: "https://www.mca.gov.cn/mzsj/xzqh/1980/201903/201903011447.html",
+    2017: "https://www.mca.gov.cn/mzsj/xzqh/1980/201803/201803131454.html",
+    2016: "https://www.mca.gov.cn/mzsj/xzqh/1980/201705/201705311652.html",
+    2015: "https://www.mca.gov.cn/mzsj/tjbz/a/2015/201706011127.html",
+    2014: "https://www.mca.gov.cn/images2/cws/201502/20150225163817214.html",
+    2013: "https://www.mca.gov.cn/images2/cws/201404/20140404125552372.htm",
+    2012: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201707271556.html",
+    2011: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201707271552.html",
+    2010: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220946.html",
+    2009: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220943.html",
+    2008: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220941.html",
+    2007: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220939.html",
+    2006: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220936.html",
+    2005: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220935.html",
+    2004: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220930.html",
+    2003: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220928.html",
+    2002: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220927.html",
+    2001: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220925.html",
+    2000: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220923.html",
+    1999: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220921.html",
+    1998: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220918.html",
+    1997: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220916.html",
+    1996: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220914.html",
+    1995: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220913.html",
+    1994: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220911.html",
+    1993: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708041023.html",
+    1992: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220910.html",
+    1991: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708041020.html",
+    1990: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708041018.html",
+    1989: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708041017.html",
+    1988: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220903.html",
+    1987: "https://www.mca.gov.cn/mzsj/xzqh/1980/1980/201911180950.html",
+    1986: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220859.html",
+    1985: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220858.html",
+    1984: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708220856.html",
+    1983: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708160821.html",
+    1982: "https://www.mca.gov.cn/mzsj/xzqh/1980/1980/201911180942.html",
+    1981: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708041004.html",
+    1980: "https://www.mca.gov.cn/mzsj/tjbz/a/201713/201708040959.html",
+}
+
+URL_CACHE_DIR = Path("data/url_cache")
+URL_CACHE_DIR.mkdir(exist_ok=True, parents=True)
+
+
+def get_text(name: str, url: str) -> str:
+    cache_path = URL_CACHE_DIR / f"{name}.bin"
+
+    if not cache_path.exists():
+        time.sleep(1)
+        print(f"Downloading {name}: {url}")
+        response = requests.get(url)
+        response.raise_for_status()
+        cache_path.write_bytes(response.content)
+
+    return cache_path.read_bytes().decode(errors="ignore")
+
+
+def main():
+    dataset = {}
+    for year, url in URL_INDEX.items():
+        print(f"Processing {year}")
+        dataset[year] = {}
+
+        html = get_text(str(year), url)
+        soup = BeautifulSoup(html, "lxml")
+        tr_list = soup.find_all("tr")
+        tr_list = [[td for td in tr.find_all("td") if td.text != ""] for tr in tr_list]
+        tr_list = [tr for tr in tr_list if len(tr) > 0]
+        for tr in tr_list:
+            if re.match(r"\d{6}", tr[0].text) is None:
+                continue
+            code = tr[0].text.strip()
+            name = tr[1].text.strip()
+            dataset[year][code] = name
+
+    with open("src/region.json", "w") as f:
+        json.dump(dataset, f, ensure_ascii=False, indent=2)
+
+
+if __name__ == "__main__":
+    main()
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,235 @@
+//! 中华人民共和国公民身份号码 (GB 11643-1999)
+
 #![deny(unsafe_code)]
 #![deny(clippy::all, clippy::pedantic, clippy::cargo)]
+#![allow(
+    clippy::inline_always,
+    clippy::missing_errors_doc, // TODO
+)]
 // ---
 #![cfg_attr(docsrs, feature(doc_cfg))]
+
+use core::fmt;
+use core::str;
+use std::collections::HashMap;
+use std::sync::LazyLock;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum Error {
+    InvalidLength,
+    InvalidCharacter,
+    WrongCheckNumber,
+    InvalidBirthday,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <Self as fmt::Debug>::fmt(self, f)
+    }
+}
+
+impl core::error::Error for Error {}
+
+pub struct ParsedIdNumber {
+    id: [u8; 18],
+    birthday: (u16, u8, u8),
+    region: Region,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Sex {
+    Male,
+    Female,
+}
+
+impl ParsedIdNumber {
+    #[must_use]
+    pub fn sex(&self) -> Sex {
+        if self.id[16] & 1 == 1 {
+            Sex::Male
+        } else {
+            Sex::Female
+        }
+    }
+
+    #[must_use]
+    pub fn birthday_ymd(&self) -> (u16, u8, u8) {
+        self.birthday
+    }
+
+    #[must_use]
+    pub fn region(&self) -> &Region {
+        &self.region
+    }
+}
+
+impl TryFrom<&str> for ParsedIdNumber {
+    type Error = Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        parse(value)
+    }
+}
+
+#[allow(clippy::needless_range_loop)]
+pub fn parse(id_str: &str) -> Result<ParsedIdNumber, Error> {
+    let id: [u8; 18] = id_str
+        .as_bytes()
+        .try_into()
+        .map_err(|_| Error::InvalidLength)?;
+
+    for i in 0..17 {
+        if !id[i].is_ascii_digit() {
+            return Err(Error::InvalidCharacter);
+        }
+    }
+    if !id[17].is_ascii_digit() && id[17] != b'X' {
+        return Err(Error::InvalidCharacter);
+    }
+
+    {
+        const W: [u8; 17] = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+        let mut sum: u32 = if id[17] == b'X' {
+            10
+        } else {
+            u32::from(id[17] - b'0')
+        };
+        for i in 0..17 {
+            sum += u32::from(id[i] - b'0') * u32::from(W[i]);
+            sum %= 11;
+        }
+        if sum != 1 {
+            return Err(Error::WrongCheckNumber);
+        }
+    }
+
+    let birthday = {
+        let year = u16_from_char4([id[6], id[7], id[8], id[9]]);
+        let month = u8_from_char2([id[10], id[11]]);
+        let day = u8_from_char2([id[12], id[13]]);
+
+        // Please change it after I'm 200 years old :)
+        if year <= 1800 || year >= 2200 {
+            return Err(Error::InvalidBirthday);
+        }
+
+        if !validate_ymd(year, month, day) {
+            return Err(Error::InvalidBirthday);
+        }
+
+        (year, month, day)
+    };
+
+    let region = {
+        let region_code = [id[0], id[1], id[2], id[3], id[4], id[5]];
+        get_region(region_code, birthday.0)
+    };
+
+    Ok(ParsedIdNumber {
+        id,
+        birthday,
+        region,
+    })
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Region {
+    pub province: Option<&'static str>,
+    pub city: Option<&'static str>,
+    pub district: Option<&'static str>,
+}
+
+fn get_region(region_code: [u8; 6], year: u16) -> Region {
+    static DATASET: LazyLock<HashMap<u16, HashMap<&'static str, &'static str>>> =
+        LazyLock::new(|| serde_json::from_str(include_str!("region.json")).unwrap());
+
+    let c = &region_code;
+    let t1 = [c[0], c[1], b'0', b'0', b'0', b'0'];
+    let t1 = str::from_utf8(&t1).unwrap();
+    let t2 = [c[0], c[1], c[2], c[3], b'0', b'0'];
+    let t2 = str::from_utf8(&t2).unwrap();
+    let t3 = str::from_utf8(&region_code).unwrap();
+
+    let dataset = &*DATASET;
+
+    if t1 == t3 {
+        if let Some(data) = dataset.get(&year) {
+            let province = data.get(t1).copied();
+            return Region {
+                province,
+                city: None,
+                district: None,
+            };
+        }
+    } else if let Some(data) = dataset.get(&year) {
+        let province = data.get(t1).copied();
+        let city = data.get(t2).copied();
+        let district = data.get(t3).copied();
+        return Region {
+            province,
+            city,
+            district,
+        };
+    }
+
+    Region {
+        province: None,
+        city: None,
+        district: None,
+    }
+}
+
+fn validate_ymd(year: u16, month: u8, day: u8) -> bool {
+    let month: time::Month = match month.try_into() {
+        Ok(m) => m,
+        Err(_) => return false,
+    };
+
+    time::Date::from_calendar_date(i32::from(year), month, day).is_ok()
+}
+
+#[inline(always)]
+fn u16_from_char4(c: [u8; 4]) -> u16 {
+    u16::from(c[0] - b'0') * 1000
+        + u16::from(c[1] - b'0') * 100
+        + u16::from(c[2] - b'0') * 10
+        + u16::from(c[3] - b'0')
+}
+
+#[inline(always)]
+fn u8_from_char2(c: [u8; 2]) -> u8 {
+    (c[0] - b'0') * 10 + (c[1] - b'0')
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        {
+            let id = "11010519491231002X";
+            let parsed = parse(id).unwrap();
+            assert_eq!(parsed.birthday, (1949, 12, 31));
+            assert_eq!(parsed.sex(), Sex::Female);
+        }
+
+        {
+            let id = "440524188001010014";
+            let parsed = parse(id).unwrap();
+            assert_eq!(parsed.birthday, (1880, 1, 1));
+            assert_eq!(parsed.sex(), Sex::Male);
+        }
+
+        {
+            let id = "420111198203251029";
+            let parsed = parse(id).unwrap();
+            assert_eq!(parsed.birthday, (1982, 3, 25));
+            assert_eq!(parsed.sex(), Sex::Female);
+            assert_eq!(parsed.region().province, Some("湖北省"));
+            assert_eq!(parsed.region().city, Some("武汉市"));
+            assert_eq!(parsed.region().district, Some("洪山区"));
+        }
+    }
+}
